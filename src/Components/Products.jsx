b46a7a1e12086e31dea909f2e3e1d8da8f24061d
moatazgamal100 <moatazfahmy100100@gmail.com>
2024-03-06 06:42:31 +0200

    lap 2

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react'
 
 export default function Products({ catags }) {
+    // console.log(catags);
     return (
         <>
             { catags.map((product,index)=>
-                <div key={product.id} className='col-4'>
-                    <img src={product.thumbnail} className='w-100'  />
+                <div key={index} className='col-md-4 mt-2' >
+                    <img src={product.thumbnail} className='w-75 bg-danger p-1'  />
                     <h1>{product.title}</h1>
-                    <p>{product.description}</p>
-                    <p>stock : {product.stock}</p>
+                    <p>{product.price}</p>
+                    <p>{product.category}</p>
+                    <button className='btn btn-danger w-25 me-4'>Delete</button>
+                    <button className='btn btn-info w-25'>Update</button>
                 </div>
             )}
 
